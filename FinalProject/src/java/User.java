09eef512eea822b536961b895becb30628b47302
David Carl <David.martin.carl@gmail.com>
2017-01-11 09:40:32 +0100

    Uploading user

---
+++ FinalProject/src/java/User.java
@@ -0,0 +1,40 @@
+
+
+public class User {
+
+    private String Username;
+    private String Password;
+    private String passwordSalt;
+    private String Email;
+    private int PhoneNo;
+
+    public User(String Username, String Password, String passwordSalt, String Email, int PhoneNo) {
+        this.Username = Username;
+        this.Password = Password;
+        this.passwordSalt = passwordSalt;
+        this.Email = Email;
+        this.PhoneNo = PhoneNo;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public int getPhoneNo() {
+        return PhoneNo;
+    }
+    
+    
+}
