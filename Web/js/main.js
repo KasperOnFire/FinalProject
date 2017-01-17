d920de3315f6266fc034e94a88c9fe19c839891d
Kasper Ravn Breindal <breindal70@gmail.com>
2017-01-17 14:38:57 +0100

    templates

--- Web/js/main.js
+++ Web/js/main.js
@@ -28,7 +28,7 @@
           $imgLink.attr("src", albumString);
       })
 
-      
+
 
       $.getJSON(jsonLink, function (data) {
           var tracks = data.album.tracks;
@@ -46,3 +46,17 @@
 
       i++;
   })
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementsByClassName("tracklist")[0];
+  var span = document.getElementsByClassName("close")[0];
+  btn.onclick = function () {
+      modal.style.display = "block";
+  }
+  span.onclick = function () {
+      modal.style.display = "none";
+  }
+  window.onclick = function (event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
