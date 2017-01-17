2e96f8319aadd1ec1ffbfe31323d79493b8718aa
Kasper Ravn Breindal <breindal70@gmail.com>
2017-01-17 15:13:59 +0100

    colorchanger

--- Web/js/main.js
+++ Web/js/main.js
@@ -55,10 +55,10 @@
        }
    }
 
-   function changeColor() {
-       var colors = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4',
-           '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#FF9800', '#FF5722'
-       ];
-       var randNumb = Math.floor(Math.random() * colors.length);
-       document.getElementsByClassName("changecolor").style["color"] = colors[randNumb];
-   }
+
+   var colors = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#FF9800', '#FF5722'];
+   var randNumb = Math.floor(Math.random() * colors.length);
+   var newColor = colors[randNumb];
+   $(document).ready(function () {
+       $(".changecolor").css("color", newColor);
+   })
